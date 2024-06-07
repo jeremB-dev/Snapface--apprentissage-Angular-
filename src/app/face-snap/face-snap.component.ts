a512d29c63b39cbac9fca754d4531937d9526260
Jerem b <jerem.brunel.33@gmail.com>
2024-06-07 15:45:40 +0200

    Premier commit

---
+++ src/app/face-snap/face-snap.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { FaceSnap } from '../models/face-snap';
+import { NgClass, NgStyle } from '@angular/common';
+
+@Component({
+  selector: 'app-face-snap',
+  standalone: true,
+  imports: [
+    NgStyle, NgClass
+  ],
+  templateUrl: './face-snap.component.html',
+  styleUrl: './face-snap.component.scss'
+})
+export class FaceSnapComponent implements OnInit {
+      @Input() faceSnap!: FaceSnap; 
+
+  snapButtonText!: string;
+  userHasSnapped!: boolean;
+
+  ngOnInit(): void {
+    this.snapButtonText = `Oh Snap !!`;
+    this.userHasSnapped = false;
+}
+
+  onSnap(): void {
+    if (this.userHasSnapped) {
+      this.unSnap();
+    } else {
+      this.snap();
+    }
+  }
+
+  unSnap() {
+      this.faceSnap.removeSnap();
+      this.snapButtonText = `Oh Snap !!`;
+      this.userHasSnapped = false;
+  }
+
+  snap () {
+    this.faceSnap.addSnap();
+    this.snapButtonText = `Oops, unSnap !!`;
+    this.userHasSnapped = true;
+  }
+}
